4f640676431903d2f0765bdddf02a6fed54b7d36
LIU Sicheng <scliu.leo@gmail.com>
2025-04-23 11:01:14 +0800

    remove prettier

--- vue-project-ts/eslint.config.ts
+++ vue-project-ts/eslint.config.ts
@@ -1,7 +1,6 @@
 import { globalIgnores } from 'eslint/config'
 import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
 import pluginVue from 'eslint-plugin-vue'
-import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
 
 // To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
 // import { configureVueProject } from '@vue/eslint-config-typescript'
@@ -18,5 +17,4 @@ export default defineConfigWithVueTs(
 
   pluginVue.configs['flat/essential'],
   vueTsConfigs.recommended,
-  skipFormatting,
 )
