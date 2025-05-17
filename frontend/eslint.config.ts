fa230693720fd26d5718801ae40d2941d4c21026
Sicheng LIU <scliu.leo@gmail.com>
2025-05-18 07:29:11 +0800

    ci(frontend): set up linting and formatting for the frontend
    - Add Husky and lint-staged for pre-commit linting
    - Update ESLint configuration with new rules and plugins
    - Modify package.json to include prepare script and lint-staged config
    - Add .husky/pre-commit file for frontend linting

--- frontend/eslint.config.ts
+++ frontend/eslint.config.ts
@@ -1,7 +1,8 @@
-import { globalIgnores } from 'eslint/config'
-import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
-import pluginVue from 'eslint-plugin-vue'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
+import { globalIgnores } from 'eslint/config'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+import pluginVue from 'eslint-plugin-vue'
 
 // To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
 // import { configureVueProject } from '@vue/eslint-config-typescript'
@@ -19,4 +20,28 @@ export default defineConfigWithVueTs(
   pluginVue.configs['flat/essential'],
   vueTsConfigs.recommended,
   skipFormatting,
+
+  {
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      indent: ['warn', 2],
+      'linebreak-style': ['error', 'unix'],
+      quotes: ['warn', 'single'],
+      // 'vue/no-v-model-argument': ['off'],
+      // 'vue/no-multiple-template-root': ['off'],
+      // 'vue/multi-word-component-names': ['off'],
+      // '@typescript-eslint/no-explicit-any': ['off'],
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+        },
+      ],
+      // '@typescript-eslint/no-non-null-assertion': ['off'],
+    },
+  },
 )
